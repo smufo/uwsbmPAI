115ae2cdf88aedf747d8ba8fec0f7a56e0943ab1
smufo <piotrkolenda23@gmail.com>
2026-04-17 14:21:48 +0200

    added other db views (plain text and table)

--- php2/show_users_obiektowe.php
+++ php2/show_users_obiektowe.php
@@ -17,26 +17,59 @@
     }
 
     if ($result->num_rows > 0) {
+        echo "Liczba rekordów: " . $result->num_rows . "<br><br>";
+        $i = 1;
         while ($row = $result->fetch_assoc()) {
             echo <<< DATA
-                ID: $row[id], imie: $row[first_name], nazwisko: $row[last_name] data urodzenia: $row[birthday]<hr>
+                Użytkownik: $i<br>
+                ID: $row[id]<br>
+                Imię: $row[first_name]<br>
+                Nazwisko: $row[last_name]<br>
+                Data urodzenia: $row[birthday]<br>
+                -----------------------------------<br>
 DATA;
+            $i++;
         }
     } else {
         echo "Brak danych w tabeli";
     }
 
-    $connection->close();
+    $result->data_seek(0);
 
+    if ($result->num_rows > 0) {
+        $result->data_seek(0);
+        echo "<table style='border: 1px solid; border-collapse: collapse; text-align: left; width: 100%;'>";
+        echo "<thead>";
+
+        echo "<tr>";
+        echo "<th colspan='5' style='border: 1px solid; text-align: center'>";
+        echo "Liczba użytkowników: " . $result->num_rows;
+        echo "  </th>";
+        echo "</tr>";
+
+        echo "<tr>
+                <th>ID</th>
+                <th>Imię</th>
+                <th>Nazwisko</th>
+                <th>Data urodzenia</th>
+              </tr>
+              </thead>";
+        echo "<tbody>";
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>";
+            echo "<td >" . $row['id'] . "</td>";
+            echo "<td>" . $row['first_name'] . "</td>";
+            echo "<td>" . $row['last_name'] . "</td>";
+            echo "<td>" . $row['birthday'] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</tbody>";
+        echo "</table>";
+    } else {
+        echo "Brak danych w tabeli";
+    }
+
+$connection->close();
 ?>
-
-<!--
-Liczba rekordów: 3
-
-Użytkownik: 1
-ID: 1
-Imię: Jan
-Nazwisko: Kowalski
-Data urodzenia: 2001-01-01
----------------------------------
--->
